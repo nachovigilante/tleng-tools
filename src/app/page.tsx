e3d7f40f75d83ab8e1f805776692fcf52e04667c
nachovigilante <nachovigilante@gmail.com>
2023-11-04 16:36:11 -0300

    fix: made brackets standout from bracket symbols

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,12 +26,16 @@ const Tablas = () => {
                             {Object.keys(siguientes).map((v) => (
                                 <tr key={v}>
                                     <td className="px-3 py-2 border">{v}</td>
-                                    <td className="px-3 py-2 border">{`{${primeros[
-                                        v
-                                    ].join(', ')}}`}</td>
-                                    <td className="px-3 py-2 border">{`{${siguientes[
-                                        v
-                                    ].join(', ')}}`}</td>
+                                    <td className="px-3 py-2 border flex items-center">
+                                        <p className="text-3xl">{`{`}</p>
+                                        {primeros[v].join(', ')}
+                                        <p className="text-3xl">{`}`}</p>
+                                    </td>
+                                    <td className="px-3 py-2 border flex items-center">
+                                        <p className="text-3xl">{`{`}</p>
+                                        {siguientes[v].join(', ')}
+                                        <p className="text-3xl">{`}`}</p>
+                                    </td>
                                 </tr>
                             ))}
                         </thead>

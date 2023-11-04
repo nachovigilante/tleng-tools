caf21ca127b088004348959e2bbd2502c2ccf09d
nachovigilante <nachovigilante@gmail.com>
2023-11-04 16:39:08 -0300

    fix: minor style fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,15 +26,19 @@ const Tablas = () => {
                             {Object.keys(siguientes).map((v) => (
                                 <tr key={v}>
                                     <td className="px-3 py-2 border">{v}</td>
-                                    <td className="px-3 py-2 border flex items-center">
-                                        <p className="text-3xl">{`{`}</p>
-                                        {primeros[v].join(', ')}
-                                        <p className="text-3xl">{`}`}</p>
+                                    <td className="px-3 py-2 border">
+                                        <div className="flex items-center">
+                                            <p className="text-3xl">{`{`}</p>
+                                            {primeros[v].join(', ')}
+                                            <p className="text-3xl">{`}`}</p>
+                                        </div>
                                     </td>
-                                    <td className="px-3 py-2 border flex items-center">
-                                        <p className="text-3xl">{`{`}</p>
-                                        {siguientes[v].join(', ')}
-                                        <p className="text-3xl">{`}`}</p>
+                                    <td className="px-3 py-2 border">
+                                        <div className="flex items-center">
+                                            <p className="text-3xl">{`{`}</p>
+                                            {siguientes[v].join(', ')}
+                                            <p className="text-3xl">{`}`}</p>
+                                        </div>
                                     </td>
                                 </tr>
                             ))}

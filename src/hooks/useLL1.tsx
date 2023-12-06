36df15856a81e80e11d8b47ea4c1882ef500b2b6
sponja23 <tomas.spognardi@gmail.com>
2023-12-06 03:18:56 -0300

    refactor(ll1): Refactored LL1 table computation

--- src/hooks/useLL1.tsx
+++ src/hooks/useLL1.tsx
@@ -1,26 +1,21 @@
-import { useEffect, useState } from 'react';
-import { Grammar } from '~/contexts/GrammarContext';
-import { TableType } from '~/utils/ps';
+import { useMemo } from 'react';
 import useGrammar from './useGrammar';
-import { Table2DType, calcularLL1, calcularSD } from '~/utils/LL1';
+import { calcularLL1, calcularSD } from '~/utils/LL1';
 
 const useLL1 = () => {
     const { Vt, Vn, prods, primeros, siguientes } = useGrammar();
 
-    const [sd, setSd] = useState<TableType>({} as TableType);
-    const [ll1, setLl1] = useState<Table2DType>({} as Table2DType);
+    const sd = useMemo(() => {
+        if (Vn.length == 0 || Vt.length == 0) return {};
+        if (prods.some((p) => !Vn.includes(p.head))) return {};
+        return calcularSD(prods, primeros, siguientes);
+    }, [prods, primeros, siguientes, Vn, Vt]);
 
-    useEffect(() => {
-        if (Vn.length == 0 || Vt.length == 0) return;
-        if (prods.some((p) => !Vn.includes(p.head))) return;
-        setSd(calcularSD(prods, primeros, siguientes));
-    }, [siguientes]);
-
-    useEffect(() => {
-        if (Vn.length == 0 || Vt.length == 0) return;
-        if (prods.some((p) => !Vn.includes(p.head))) return;
-        setLl1(calcularLL1(Vt, Vn, sd));
-    }, [sd]);
+    const ll1 = useMemo(() => {
+        if (Vn.length == 0 || Vt.length == 0) return {};
+        if (prods.some((p) => !Vn.includes(p.head))) return {};
+        return calcularLL1(Vt, Vn, sd);
+    }, [Vn, Vt, sd, prods]);
 
     return { sd, ll1 };
 };
